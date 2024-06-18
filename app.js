47e7c37a58f43b8d69754dbd7e377b46b608409a
mikey <mikey@hey.com>
2024-06-18 13:28:18 -0500

    Integrated Socket.io

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const express = require('express');
 const app = express();
 // Socket.io has to use the http server
 const server = require('http').Server(app);
+const io = require('socket.io')(server);
+io.on('connection', (socket) => {
+  console.log('🔌 New user connected! 🔌');
+});
 // Express View Engine for Handlebars
 const { engine } = require('express-handlebars');
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
+
+//Establish your public folder
+app.use('/public', express.static('public'));
 
 app.get('/', (req, res) => {
   res.render('index.handlebars');
